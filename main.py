ddae391c4c0e9a6d3e2a31029d63817658d33e37
juliethrojas755-beep <juliethrojas755@gmail.com>
2026-03-02 11:18:11 -0500

    funcion saludo

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ print ("Correccion aplicada")
 print("Hotfix v1.0.1: corrección menor aplicada")
 print("Hotfix v1.0.1: corrección menor aplicada")
 print("Proyecto Git Flow inicial")
+
+# main.py
+def saludar():
+    print("¡Bienvenido a Git Flow!")
